b2e2c57b5454d7a3355e506a906beab02e2c5f3e
simonqq21 <ques86225@gmail.com>
2024-12-03 10:52:31 +0800

    working example of buttons with interrupts with two buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,90 @@
 #include <Arduino.h>
+#include "buttonlib2.h"
 
-const int debounceDelay = 50;
-const int multiClickDuration = 500;
-const int longClickDuration = 1000;
-
-const int BTN1 = 2;
-const int BTN2 = 3;
-
-bool btn1_curState;
-bool btn1_changed;
-unsigned long btn1LastDebounceTime;
-unsigned long lastClickTime;
-int btn1NumClicks;
-
-int i1=0;
-
-
-
-
-
-// void btn1_falling_func() {
-//   btn1_pressed = true;
-//   // button1releasedtime = millis();
-// }
-
-// void btn1_rising_func() {
-//   btn1_released = true;
-//   // button1pressedtime = millis();
-//   digitalWrite(LED_BUILTIN, LOW);
-// }
+const int btn1pin = 2;
+const int btn2pin = 3;
+InterruptButton btn1(btn1pin);
+InterruptButton btn2(btn2pin);
 
 void btn1_change_func() {
-  btn1_changed = true;
-  btn1LastDebounceTime = millis();
+  btn1.changeInterruptFunc();
 }
 
-// void btn2_falling_func() {
-//   btn2_pressed = true;
-//   digitalWrite(LED_BUILTIN, HIGH);
-// }
+void btn2_change_func() {
+  btn2.changeInterruptFunc();
+}
 
-// void btn2_rising_func() {
-//   btn2_released = true;
-// }
+void btn1_1shortclick() {
+  Serial.println("btn1 clicked 1 time");
+}
+
+void btn1_2shortclick() {
+  Serial.println("btn1 clicked 2 times");
+}
+
+void btn1_3shortclick() {
+  Serial.println("btn1 clicked 3 times");
+}
+
+void btn1_1longclick() {
+  Serial.println("btn1 long pressed 1 time");
+}
+
+void btn1_2longclick() {
+  Serial.println("btn1 long pressed 2 times");
+}
+
+void btn1_3longclick() {
+  Serial.println("btn1 long pressed 3 times");
+}
+
+
+void btn2_1shortclick() {
+  Serial.println("btn2 clicked 1 time");
+}
+
+void btn2_2shortclick() {
+  Serial.println("btn2 clicked 2 times");
+}
+
+void btn2_3shortclick() {
+  Serial.println("btn2 clicked 3 times");
+}
+
+void btn2_1longclick() {
+  Serial.println("btn2 long pressed 1 time");
+}
+
+void btn2_2longclick() {
+  Serial.println("btn2 long pressed 2 times");
+}
+
+void btn2_3longclick() {
+  Serial.println("btn2 long pressed 3 times");
+}
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BTN1, INPUT_PULLUP);
-  pinMode(BTN2, INPUT_PULLUP);
+  btn1.begin(btn1_change_func);
+  btn2.begin(btn2_change_func);
   pinMode(LED_BUILTIN, OUTPUT);
-  // attachInterrupt(digitalPinToInterrupt(BTN1), btn1_falling_func, FALLING);
-  // attachInterrupt(digitalPinToInterrupt(BTN1), btn1_rising_func, RISING);
-  attachInterrupt(digitalPinToInterrupt(BTN1), btn1_change_func, CHANGE);
+
+  btn1.set1ShortPressFunc(btn1_1shortclick);
+  btn1.set2ShortPressFunc(btn1_2shortclick);
+  btn1.set3ShortPressFunc(btn1_3shortclick);
+  btn1.set1LongPressFunc(btn1_1longclick);
+  btn1.set2LongPressFunc(btn1_2longclick);
+  btn1.set3LongPressFunc(btn1_3longclick);
+
+  btn2.set1ShortPressFunc(btn2_1shortclick);
+  btn2.set2ShortPressFunc(btn2_2shortclick);
+  btn2.set3ShortPressFunc(btn2_3shortclick);
+  btn2.set1LongPressFunc(btn2_1longclick);
+  btn2.set2LongPressFunc(btn2_2longclick);
+  btn2.set3LongPressFunc(btn2_3longclick);
 }
 
 void loop() {
-  if (btn1_changed && millis() - btn1LastDebounceTime > debounceDelay) {
-    btn1_curState = digitalRead(BTN1);
-
-    /**
-     * when pressed, increment the btn click count if within the multiclick delay.
-     */
-    if (!btn1_curState) {
-      Serial.print("btn1 pressed ");
-      lastClickTime = millis();
-      btn1NumClicks++;
-      Serial.println(i1);
-      i1++;
-    }
-    // else Serial.print("btn1 released ");
-    btn1_changed = false;
-  }
-
-  if (btn1NumClicks) {
-    if (millis() - lastClickTime > multiClickDuration) {
-      if (digitalRead(BTN1)) {
-        Serial.print("button clicked ");
-        Serial.print(btn1NumClicks);
-        Serial.println(" times.");
-        btn1NumClicks = 0; 
-      }
-    }
-    if (millis() - lastClickTime > longClickDuration) {
-      if (!digitalRead(BTN1)) {
-        Serial.print("button long clicked ");
-        Serial.print(btn1NumClicks);
-        Serial.println(" times.");
-        btn1NumClicks = 0; 
-      }
-    }
-  }
-
-  // Serial.println(millis() - lastClickTime);
-  
-
-  // if (btn1NumClicks && )
-  // if (btn1_released) {
-  //   Serial.println("button released");
-  //   // Serial.print("button1releasedtime = ");
-  //   // Serial.print(button1releasedtime);
-  //   // Serial.print(" button1pressedtime = ");
-  //   // Serial.print(button1pressedtime);
-  //   // Serial.print(" pressed time = ");
-  //   // Serial.println(button1releasedtime - button1pressedtime);
-  //   btn1_released = false;
-  // }
-
-  // if (btn1_pressed) {
-  //   Serial.println("button pressed");
-  //   btn1_pressed = false;
-  // }
-
-  // if (btn2_pressed) {
-  //   Serial.println("f2");
-  //   Serial.println(i2);
-  //   i2++;
-  //   btn2_pressed = false;
-  // }
-  
-  
+  btn1.loop();
+  btn2.loop();
 }
